aff99c9f6a12ea4d4df38093d0706370c0610c0c
omar <omasrt@gmail.com>
2019-05-24 12:39:34 -0500

    add Oauth

---
+++ routes/drivers.js
@@ -0,0 +1,15 @@
+var router = require('express').Router();
+var driversCtrl = require('../controllers/drivers');
+
+router.get('/drivers', driversCtrl.index);
+
+// router.post('/team', isLoggedIn, driversCtrl.addTeam);
+
+
+
+function isLoggedIn(req, res, next) {
+  if ( req.isAuthenticated() ) return next();
+  res.redirect('/auth/google');
+}
+
+module.exports = router;
--- server.js
+++ server.js
@@ -3,24 +3,49 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
+var passport = require('passport');
+var methodOverride = require('method-override');
+
+require('dotenv').config();
+
+
+var app = express();
+
+// connect to the MongoDB with mongoose
+require('./config/database');
+// configure Passport
+require('./config/passport');
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var driversRouter = require('./routes/drivers')
 
-var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(methodOverride('_method'));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+  secret: 'Good',
+  resave: false,
+  saveUninitialized: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/', indexRouter);
+app.use('/', driversRouter);
 app.use('/users', usersRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
