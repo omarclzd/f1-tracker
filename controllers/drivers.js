1646f2ca6024d7d727e884ef46e7480951820cdd
omar <omasrt@gmail.com>
2019-07-03 00:48:17 -0500

    updated show and create controllers

--- controllers/drivers.js
+++ controllers/drivers.js
@@ -1,60 +1,53 @@
-const User = require('../models/user');
-const request = require('request');
+const User = require("../models/user");
+const request = require("request");
 
-const driversURL = 'http://ergast.com/api/f1/2019/last/results.json';
+const driversURL = "http://ergast.com/api/f1/2019/last/results.json";
 
 module.exports = {
   create,
-new: newDriver,
-show,
-delDriver
+  new: newDriver,
+  show,
+  delDriver
 };
 
 function show(req, res) {
- 
-  
-      User.findById(req.params.id, req.body, function(err, team) { 
-        let red = req.params.id;
-        console.log(red);
-        console.log(req.body);
-        res.render('teams/show', {
-          title: 'Edit Team',
-          user: req.user,
-          red,
-           
-        });
-      });
-  
-  }
+  User.findById(req.params.id, req.body, function(err, team) {
+    let red = req.params.id;
+    console.log(red);
+    console.log(req.body);
+    res.render("teams/show", {
+      title: "Edit Team",
+      user: req.user,
+      red
+    });
+  });
+}
 
 function create(req, res) {
   User.findById(req.session.passport.user, function(err, user) {
     user.drivers.push(req.body);
     user.save(function(err) {
-      res.redirect('/teams');
+      res.redirect("/teams");
     });
-    
   });
-};
-
+}
 
 function newDriver(req, res) {
   request(driversURL, function(err, response, body) {
     const driverData = JSON.parse(body);
     // console.log(teamData.MRData.ConstructorTable.Constructors);
-  res.render('drivers/new', {
-    user: req.user,
-    driverData: driverData.MRData.RaceTable.Races[0],
-    
-  });
+    res.render("drivers/new", {
+      user: req.user,
+      driverData: driverData.MRData.RaceTable.Races[0]
+    });
   });
 }
 
 function delDriver(req, res) {
-  User.findOne({'drivers._id': req.params.id}, function(err, user) {
+  User.findOne({ "drivers._id": req.params.id }, function(err, user) {
     user.drivers.id(req.params.id).remove();
     user.save(function(err) {
-      res.redirect('/teams');
+      res.redirect("/teams");
     });
   });
 }
--- controllers/teams.js
+++ controllers/teams.js
@@ -1,8 +1,8 @@
-const User  = require('../models/user');
+const User = require("../models/user");
 // const Driver = require('../models/driver');
-const request = require('request');
-const driversURL = 'http://ergast.com/api/f1/2019/last/results.json';
-const teamsURL = 'http://ergast.com/api/f1/2019/constructorStandings.json';
+const request = require("request");
+const driversURL = "http://ergast.com/api/f1/2019/last/results.json";
+const teamsURL = "http://ergast.com/api/f1/2019/constructorStandings.json";
 
 module.exports = {
   index,
@@ -11,99 +11,79 @@ module.exports = {
   show,
   delTeam,
   edit,
-  updateTeam,
+  updateTeam
 };
 
 function show(req, res) {
   request(teamsURL, function(err, response, body) {
     const teamData = JSON.parse(body);
     // console.log(teamData.MRData.StandingsTable.StandingsLists[0].ConstructorStandings);
-      User.find(req.body, function(err, user) { 
-        let red = req.params.id;
-        console.log(red);
-        
-        user.forEach(function(i) {
-          console.log(user[0].teams[0].name);
-          
-        });
-        
-        res.render('teams/show', {
-          title: 'Edit Team',
-          user: req.user,
-          red,
-          teamData: teamData.MRData.StandingsTable.StandingsLists[0].ConstructorStandings,     
-        });
-      });
-    });
-  }
-        
-  function create(req, res) {  
-    request(teamsURL, function(err, response, body) {
-      const teamData = JSON.parse(body); 
-      console.log(teamData.MRData.StandingsTable.StandingsLists[0].ConstructorStandings[0].Constructor);
-    User.findById(req.session.passport.user, function(err, user) { 
-      console.log()                           
-        user.teams.push(req.body);
-        user.save(function(err) {
-          res.render('teams/show', {
-            user: req.user,
-            teamData: teamData.MRData.StandingsTable.StandingsLists[0].ConstructorStandings[0].Constructor, 
-            
-          });   
-        });
-    });          
-    });
-  }
-          
-  function newTeam(req, res) {
-    request(teamsURL, function(err, response, body) {
-      const teamData = JSON.parse(body);
-      // console.log(teamData.MRData.ConstructorStandings);
-      res.render('teams/new', {
+    User.findById(req.params.id, req.body, function(err, team) {
+      let red = req.params.id;
+      console.log(red);
+      console.log(req.body);
+      res.render("teams/show", {
+        title: "Edit Team",
         user: req.user,
-        
+        red,
+        teamData:
+          teamData.MRData.StandingsTable.StandingsLists[0].ConstructorStandings
       });
     });
-  }
-                
-                
-    function index(req, res, next) {
-      request(driversURL, function(err, response, body) {
-        const driverData = JSON.parse(body);
-        User.findById(req.params.id, function(err, team) {
-          res.render('teams/index', {
-            user: req.user,
-            driverData: driverData.MRData.RaceTable.Races[0],
-          });
-        }); 
-      });
-    }
-                
-                
-  function delTeam(req, res, next) {
-    User.findOne({'teams._id': req.params.id}, function(err, user) {
-      user.teams.id(req.params.id).remove();
-      user.save(function(err) {
-        res.redirect('/teams');
+  });
+}
+
+function create(req, res) {
+  User.findById(req.session.passport.user, function(err, user) {
+    user.teams.push(req.body);
+    user.save(function(err) {
+      res.redirect("/teams");
+    });
+  });
+}
+
+function newTeam(req, res) {
+  request(teamsURL, function(err, response, body) {
+    const teamData = JSON.parse(body);
+    // console.log(teamData.MRData.ConstructorStandings);
+    res.render("teams/new", {
+      user: req.user
+    });
+  });
+}
+
+function index(req, res, next) {
+  request(driversURL, function(err, response, body) {
+    const driverData = JSON.parse(body);
+    User.findById(req.params.id, function(err, team) {
+      res.render("teams/index", {
+        user: req.user,
+        driverData: driverData.MRData.RaceTable.Races[0]
       });
     });
-  }
-                
- function edit(req, res) {
-   User.findById(req.params.id, function(err, user) {
-        // let red = req.params.id;
-        res.render('teams/edit', { user: req.user});
-      });
-    }
+  });
+}
+
+function delTeam(req, res, next) {
+  User.findOne({ "teams._id": req.params.id }, function(err, user) {
+    user.teams.id(req.params.id).remove();
+    user.save(function(err) {
+      res.redirect("/teams");
+    });
+  });
+}
+
+function edit(req, res) {
+  User.findById(req.params.id, function(err, user) {
+    // let red = req.params.id;
+    res.render("teams/edit", { user: req.user });
+  });
+}
 
 function updateTeam(req, res, next) {
   User.findByIdAndUpdate(req.params.id, req.body, function(err, user) {
     user.save(function(err) {
-        res.redirect('/teams')
+      res.redirect("/teams");
     });
-  }); 
+  });
 }
-    
-  
-  
-  
